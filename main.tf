8ef6cfc561965a6ce41e40fe3b9b5e842d93cfc6
Sameer <101310186+Sameer138@users.noreply.github.com>
2026-02-24 23:11:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
-  AWS_ACCESS_KEY_ID = "AKIAVAUKT66Q2FAQYQZ3"
-  AWS_SECRET_ACCESS_KEY = "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"
 }
 
 resource "aws_s3_bucket" "demo" {
