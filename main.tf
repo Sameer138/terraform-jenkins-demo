1695585c17fe72a34e861e5d71532f4326facda1
Sameer <101310186+Sameer138@users.noreply.github.com>
2026-02-24 12:29:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
+  AWS_SECRET_ACCESS_KEY = "AKIAVAUKT66Q2FAQYQZ3"
+  AWS_SECRET_ACCESS_KEY = "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"
 }
 
 resource "aws_s3_bucket" "demo" {
